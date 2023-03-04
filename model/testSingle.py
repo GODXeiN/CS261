0efc5804be20cdbcd3606f9f0517b15064af7662
Billy Pentney <64329402+Billy-Pentney@users.noreply.github.com>
2023-03-04 16:20:27 +0000

    Remove unused comments from testSingle

--- model/testSingle.py
+++ model/testSingle.py
@@ -25,33 +25,3 @@ pSample = p.get_labelled_samples(1).iloc[0]
 # Generate the Risk-Assessment and display it
 ra = rag.generate_ra(pSample)
 print(ra)
-
-# Get the prediction 
-# p_pred = pipeLR.predict(proj_x)
-
-# # Get the confidence of the model in each outcome ([0] = failure, [1] = success)
-# confProbs = pipeLR.predict_proba(proj_x)[0]
-# print("\nConfidence Probabilities:", str(confProbs))
-
-# # Load the saved accuracy of the model, so we can determine the likelihood of its prediction being accurate
-# accuracyFile = open(OVERALL_MODEL_ACCURACY_DEST, 'r')
-# mdlAccuracy = float(accuracyFile.read())
-# accuracyFile.close()
-
-# # Display result of prediction
-# if p_pred[0] == 1:
-#     print(" >>> LogReg prediction: (1-Success)")
-#     print(" >>> Model Confidence:", confProbs[1])
-# elif p_pred[0] == 0:
-#     print(" >>> LogReg prediction: (0-Failure)")
-#     print(" >>> Model Confidence:", confProbs[0])
-# else:
-#     print(" >>> LogReg prediction: Cancelled (-1)")
-
-# # Determine the overall chance of success by using Conditional Probability & Bayes Formula
-# # SUCCESS = MODEL_SUCCESS * MODEL_ACCURATE + (1-MODEL_FAILURE) * MODEL_INACCURATE
-# overallSuccessEst = confProbs[1] * mdlAccuracy + confProbs[0] * (1-mdlAccuracy)
-# # Convert estimation to a probability
-# overallSuccessProb = round(100 * overallSuccessEst, 2)
-
-# print("\nOverall Estimation of Success:", str(overallSuccessProb) + "%")
